76c4e84b5893ae657704e9dbce5d80e3713b1787
OdairDias <odair2d@hotmail.com>
2022-11-16 18:49:09 -0300

    ajuste entrada data
    Signed-off-by: OdairDias <odair2d@hotmail.com>

--- leitura_modelo.py
+++ leitura_modelo.py
@@ -6,9 +6,10 @@ import pandas as pd
 import numpy as np
 #
 ativo = "PETR4.SA"  # https://finance.yahoo.com/
-dados_do_ativo = dados.DataReader(ativo, start="2018-01-01",end="2022-11-05", data_source='yahoo')
+# iremos colocar data inicial, sistema irá buscar até o dia atual
+dados_do_ativo = dados.DataReader(ativo, start="2019-01-01", data_source='yahoo')
 import matplotlib.pyplot as plt
-# plt.style.use('fivethirtyeight')
+#plt.style.use('fivethirtyeight')
 # plt.style.use('default')
 plt.figure(figsize=(16,8))
 plt.title('Histórico de fechamento ' + ativo)
@@ -38,8 +39,8 @@ x_treino = np.reshape(x_treino, (len(x_treino),30,1))
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
 model = Sequential()
-model.add(LSTM(units=50, return_sequences=True,input_shape=(30,1)))
-model.add(LSTM(units=50, return_sequences=False))
+model.add(LSTM(units=70, return_sequences=True,input_shape=(30,1)))
+model.add(LSTM(units=70, return_sequences=False))
 model.add(Dense(units=25))
 model.add(Dense(units=1))
 model.compile(optimizer='adam', loss='mean_squared_error')
@@ -47,18 +48,16 @@ model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_treino, y_treino, batch_size=1, epochs=1)
 # separação e preparação dados para teste
 cotacoes_teste = cotacoes_normalizadas[dias_treinamento - 30:]
-
 x_teste = []
 y_teste =  cotacoes[dias_treinamento:] 
 for i in range(30,len(cotacoes_teste)):
     x_teste.append(cotacoes_teste[i-30:i])
-
 x_teste = np.array(x_teste)
 x_teste = np.reshape(x_teste, (len(x_teste),30,1))
 # teste predição:
 predictions = model.predict(x_teste) 
 predictions = normalizador.inverse_transform(predictions)
-# Calculo erro diferencial:
+## Calculo da Raiz do Erro Médio Quadrado (RMSE)
 rmse=np.sqrt(np.mean(((predictions- y_teste)**2)))
 rmse
 print(rmse)
@@ -66,7 +65,7 @@ print(rmse)
 treino = cotacoes_df[:dias_treinamento]
 valido = cotacoes_df[dias_treinamento:]
 valido['Predictions'] = predictions
-plt.figure(figsize=(16,8))
+plt.figure(figsize=(15,7))
 plt.title('Ativo ' + ativo)
 plt.xlabel('Data')
 plt.ylabel('Preço de fechamento (R$)')
@@ -75,3 +74,4 @@ plt.plot(valido['Close'], color = 'green')
 plt.plot(valido['Predictions'], color = 'red')
 plt.legend(['Histórico', 'Valor real', 'Predição'])
 plt.show()
+#
